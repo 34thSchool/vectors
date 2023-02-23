ebc1a14743004d4457d77d5fb5965b22077f9a32
Aleksandr Stukalov <stukalovsaleksandrs@gmail.com>
2023-02-23 22:13:41 +0200

    Made vector coordinates being exported

--- main.go
+++ main.go
@@ -10,28 +10,28 @@ import (
 
 // Vector:
 type Vec struct {
-	x, y, z float64
+	X, Y, Z float64
 }
 
 func Add(a, b Vec) Vec {
-	return Vec{a.x + b.x, a.y + b.y, 0}
+	return Vec{a.X + b.X, a.Y + b.Y, 0}
 }
 
 func Sub(a, b Vec) Vec {
-	return Vec{a.x - b.x, a.y - b.y, 0}
+	return Vec{a.X - b.X, a.Y - b.Y, 0}
 }
 
 func Divide(v Vec, a float64) Vec {
-	return Vec{v.x / a, v.y / a, 0}
+	return Vec{v.X / a, v.Y / a, 0}
 }
 
 func Mod(a Vec) float64 {
-	return math.Sqrt(a.x*a.x + a.y*a.y)
+	return math.Sqrt(a.X*a.X + a.Y*a.Y)
 }
 
 // Line:
 func DrawLine(screen *ebiten.Image, a, b Vec) {
-	ebitenutil.DrawLine(screen, a.x, a.y, b.x, b.y, color.RGBA{255, 102, 204, 255})
+	ebitenutil.DrawLine(screen, a.X, a.Y, b.X, b.Y, color.RGBA{255, 102, 204, 255})
 }
 
 // Rectangle:
